2908c5734dd6d84e616fc2c8fe919593f9f0a20f
Neuro Linker <63702962+DmitryDobr@users.noreply.github.com>
2024-07-14 18:45:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,34 +4,6 @@
     header('Access-Control-Allow-Methods: *');
     header('Access-Control-Allow-Credentials: true');
     header('Content-Type: application/json; charset=utf-8');
-    
-    $log = true;
-    if ($log) {
-        $f = fopen("./logs/" . date("Y-m-d H:i:s") . ".txt", 'a');
-        fwrite($f, json_encode($_POST));
-        
-        $f1 = fopen("./logs2/" . date("Y-m-d H:i:s") . ".txt", 'a');
-        fwrite($f1, print_r($_POST, true));
-        
-        // ------------------------------------------------------------------
-        
-        if (ISSET($_POST['leads']['status'])) {
-            
-            $f3 = fopen("./logs3/" . date("Y-m-d H:i:s") . ".txt", 'a');
-            
-            foreach ($_POST['leads']['status'] as $lead) {
-                fputs($f3, 'ID сделки     : ' . print_r($lead['id'], true) . PHP_EOL);
-                fputs($f3, 'Имя сделки    : ' . print_r($lead['name'], true) . PHP_EOL);
-                fputs($f3, 'ID воронки    : ' . print_r($lead['pipeline_id'], true) . PHP_EOL);
-                fputs($f3, 'ID пред.этапа : ' . print_r($lead['old_status_id'], true) . PHP_EOL);
-                fputs($f3, 'ID тек.этапа  : ' . print_r($lead['status_id'], true) . PHP_EOL);
-            }
-        }
-        
-        // file_put_contents('log.txt', print_r($_REQUEST, true));
-    }
-    
-    //-----------------------------------------------------
 
     require_once 'db_handler.php';
     require_once 'AmoClient.php';
@@ -54,7 +26,6 @@
     }
     
     //print_r($amo->getSettings());
-    
     print_r($amo->ApiRequest('/api/v4/account?with=task_types', null, true, false));
 
     
